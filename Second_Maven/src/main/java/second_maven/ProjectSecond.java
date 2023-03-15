06423a27d1c4718828ed15c72a444b49c6e34538
ashak <ashak@Ashak>
2023-03-15 19:46:53 +0400

    Recent Second Commit

---
+++ Second_Maven/src/main/java/second_maven/ProjectSecond.java
@@ -0,0 +1,11 @@
+package second_maven;
+
+public class ProjectSecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
